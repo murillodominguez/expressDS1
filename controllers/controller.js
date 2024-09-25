d29050bf138f4d37f29221812b9507a8a3bf6684
murillodominguez <murillo.v.carballo@gmail.com>
2024-09-25 02:45:48 -0300

    final working version, might add some fixes but everything is fine

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,39 +1,78 @@
-const {sequelize, Sequelize} = require('../config/database');
-const employeeModel = require("../models/employees")(sequelize,Sequelize);
+const { sequelize, Sequelize } = require('../config/database');
+const Op = Sequelize.Op;
+const employeeModel = require("../models/employees")(sequelize, Sequelize);
 
-exports.listAll = (req,res) => {
+exports.listAll = (req, res) => {
+    if (req.query.search === undefined) {
+        req.query.search = '';
+    }
     employeeModel.findAll(
         {
-            order:[['gross_salary','DESC']]
+            where: {
+                [Op.or]: {
+                    name: {
+                        [Op.like]: '%' + req.query.search + '%'
+                    },
+                    // gross_salary: {
+                    //     [Op.like]: req.query.search
+                    // },
+                    // liquid_salary: {
+                    //     [Op.like]: parseInt(req.query.search)
+                    // },
+                    department: {
+                        [Op.like]: '%' + req.query.search + '%'
+                    }
+                }
+            },
+            order: [['id', 'ASC']],
         }
-    ).then(results=>{
-        if(results.length != 0){
-            for(let i = 0; i < results.length; i++){
-            let depId = results[i]['department'];
-            results[i]['department'] = departmentJSON[depId];
-            results[i]['gross_salary'] = results[i]['gross_salary'].toFixed(2);
-            results[i]['liquid_salary'] = results[i]['liquid_salary'].toFixed(2);
+    ).then(results => {
+        if (results.length != 0) {
+            for (let i = 0; i < results.length; i++) {
+                let depId = results[i]['department'];
+                results[i]['department'] = departmentJSON[depId];
+                results[i]['gross_salary'] = results[i]['gross_salary'].toFixed(2);
+                results[i]['liquid_salary'] = results[i]['liquid_salary'].toFixed(2);
             }
+            //results = orderBy(results, 'gross_salary', 1);
         }
-        res.render('listAll', {data: results})
+        res.render('listAll', { data: results })
     }).catch(err => {
-        res.status(500).send({message: "Error" + err.message});
+        res.status(500).send({ message: "Error" + err.message });
+        2
     });
-
 }
 
-exports.insertForm = (req,res) => {
+exports.listOne = (req, res) => {
+    const id_param = req.params.id;
+    employeeModel.findAll({
+        where: { id: id_param }
+    }).then(result => {
+        if (!result) {
+            req.status(400).json(
+                { message: "An error occurred..." }
+            );
+        }
+        console.log(result);
+        let data = result[0].dataValues;
+        res.render('updateEmployees', { data: data });
+    }).catch(err => {
+        res.status(500).send({ message: "Error " + err.message });
+    });
+}
+
+exports.insertForm = (req, res) => {
     res.render('insertEmployees');
 }
 
-exports.insertData = (req,res) => {
+exports.insertData = (req, res) => {
     const employeeData = {
         name: req.body.name,
         gross_salary: req.body.gross_salary,
         liquid_salary: calcSalLiq(req.body.gross_salary),
         department: req.body.department
     };
-    
+
     employeeModel.create(employeeData).then(data => {
         console.log("empregado inserido");
         res.redirect('/');
@@ -49,28 +88,69 @@ const departmentJSON = {
     4: 'Fábrica'
 }
 
+exports.delete = (req, res) => {
+    const id_param = req.params.id;
+    employeeModel.destroy({
+        where: { id: id_param }
+    }).then((result) => {
+        if (!result) {
+            req.status(400).json(
+                { message: "An error occurred..." }
+            );
+        }
+        res.redirect("/");
+    }).catch((err) => {
+        res.status(500).json({ message: "Could not delete such object." });
+        console.log(err);
+    }
+    )
+}
+
+exports.update = (req, res) => {
+    employeeModel.update(
+        {
+            name: req.body.name,
+            gross_salary: req.body.gross_salary,
+            liquid_salary: calcSalLiq(req.body.gross_salary),
+            department: req.body.department
+        }, {
+        where: { id: req.body.id_for_updating }
+    }
+    ).then(anything => {
+        if (!anything) {
+            req.status(400).send({ message: "An error ocurred." })
+        }
+        res.redirect('/');
+    }).catch(err => {
+        res.status(500).send({
+            message: "Error when trying to access the database"
+        })
+    })
+
+}
+
 calcSalLiq = (employeeSalary) => {
     const INSS = employeeSalary * 0.11;
 
     let IRPF;
 
-    if(employeeSalary > 4664.68) {
+    if (employeeSalary > 4664.68) {
         IRPF = (employeeSalary - 4664.68) * 0.275 + 413.42;
     }
 
-    else if(employeeSalary > 3751.06) {
+    else if (employeeSalary > 3751.06) {
         IRPF = (employeeSalary - 3751.06) * 0.225 + 207.86;
     }
 
-    else if(employeeSalary > 2826.65) {
+    else if (employeeSalary > 2826.65) {
         IRPF = (employeeSalary - 2826.65) * 0.15 + 69.20;
     }
 
-    else if(employeeSalary > 1903.98) {
+    else if (employeeSalary > 1903.98) {
         IRPF = (employeeSalary - 1903.98) * 0.075;
     }
 
-    else{
+    else {
         IRPF = 0;
     }
 
--- routes/approutes.js
+++ routes/approutes.js
@@ -5,6 +5,10 @@ const controller = require("../controllers/controller");
 router.get('/', controller.listAll);
 router.get('/insert', controller.insertForm);
 router.post('/insert', controller.insertData);
+router.get('/edit/:id', controller.listOne);
+router.get('/delete/:id', controller.delete);
+router.post('/update', controller.update);
+// router.get('/search?:name', controller.search);
 
 // router.get('/delete/:id', controller.delete);
 // router.get('/edit/:id', controller.editForm);
